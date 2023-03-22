20bfb1ef1f71a992c35c2060a5315cc8f434a802
Lucas Stephenson <lstephenson@cegep-heritage.qc.ca>
2023-03-22 10:08:41 -0400

    example endpoint

--- routes/index.js
+++ routes/index.js
@@ -2,8 +2,13 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get('/', function (req, res, next) {
+	res.render('index', { title: 'My Cite' });
 });
+
+router.get('/:msg', function (req, res, next) {
+	res.render('index', { title: req.params.msg });
+});
+
 
 module.exports = router;
