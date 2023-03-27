9b372c31448ddf52c7909107cb9cce099f2505a9
Lucas Stephenson <lstephenson@cegep-heritage.qc.ca>
2023-03-27 11:45:12 -0400

    add logout functionality, ex of authorization

--- app.js
+++ app.js
@@ -49,21 +49,31 @@ app.use(async (req, res, next) => {
 			res.cookie('uid', req.login.user.user_id, { maxAge: 1000 * 60 * 60 * 24 * 5 });
 			// store as cookie for cookie code
 			res.cookie('ch', req.login.cookie, { maxAge: 1000 * 60 * 60 * 24 * 5 });
-			console.log("requested redirect:" + req.body.redirect);
+			//console.log("requested redirect:" + req.body.redirect);
 			if (req.body.redirect != undefined &&
 				req.body.redirect != '' &&
-				req.body.redirect != null &&
-				!req.body.redirect.isEmpty()
+				req.body.redirect != null
 			) {
 				res.redirect(req.body.redirect); // if redirect location specified... redirect there
 			} else {
 				res.redirect('/'); // redirect location is blank, redirect to home page
 			}
 		}
-	} else if (req.cookies.uid != undefined && req.cookies.ch != undefined) {
+	}
+
+	if (!req.login.loggedIn && req.cookies.uid != undefined && req.cookies.ch != undefined) {
 		// if uid and cookie code are set, try logging in with cookies
 		let auth = await user.cookieLogin(req.cookies.uid, req.cookies.ch);
 		req.login = auth;
+
+		if (req.query.logout != undefined) {
+			// clear cookies
+			req.login = { loggedIn: false };
+			res.clearCookie('uid');
+			res.clearCookie('ch');
+
+		}
+
 	}
 
 	next();
--- routes/photo.js
+++ routes/photo.js
@@ -9,22 +9,30 @@ router.get('/', async (req, res) => {
 
 	//console.log(photos.rows);
 	// render template 'index.hbs', send the menu items as "menu", photo records as "data"
-	res.render('photos', { menu: menuItems, data: photos.rows, login: req.login });
+	res.render('photos', {
+		menu: menuItems,
+		data: photos.rows,
+		login: req.login,
+		redirect: req.baseUrl + req.path
+	});
 
 });
-router.get('/:photo_id([1-9][0-9]?)', async (req, res) => {
+router.get('/:photo_id([1-9][0-9]?)', async (req, res, next) => {
 
 	const img = await photo.getPhoto(req.params.photo_id);
 	const photos = await photo.getPhotos();
 	const menuItems = await page.getMenuItems();
 
-	//console.log(photos.row);
-	res.render('photo', {
-		data: img.row,
-		menu: menuItems,
-		photos: photos.rows,
-		login: req.login
-	});
+	if (req.login.loggedIn) {
+		res.render('photo', {
+			data: img.row,
+			menu: menuItems,
+			photos: photos.rows,
+			login: req.login
+		});
+	} else {
+		res.render('notloggedin', { redirect: req.baseUrl + req.path });
+	}
 
 });
 
