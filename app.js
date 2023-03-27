93e9093b226f832258649c947fe78cae03480b86
Lucas Stephenson <lstephenson@cegep-heritage.qc.ca>
2023-03-27 10:45:53 -0400

    auth using db, login partial

--- app.js
+++ app.js
@@ -7,11 +7,13 @@ const logger = require('morgan');
 const indexRouter = require('./routes/index');
 const booksRouter = require('./routes/books');
 const photoRouter = require('./routes/photo');
+
+
+const user = require('./model/user');
 const app = express();
 
 const hbs = require('hbs');
 
-// const user = require('./model/user');
 // user.addUser("bob", "bob@bob.com", "1234");
 
 // register partials
@@ -30,21 +32,40 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 // middleware : no path -> 
 //	run for every request that reaches this line
-app.use((req, res, next) => {
-	console.log("cookie user: " + req.cookies.user);
-	if (req.query.u != undefined) { // if there is a u parameter in the qs
-		// record that username, create/store login object onto req
-		req.login = {
-			username: req.query.u.toLowerCase().trim(),
-			auth: true
-		};
-	} else {
-		req.login = {
-			username: null,
-			auth: false
-		};
+app.use(async (req, res, next) => {
+
+	req.login = { loggedIn: false };
+
+	if (req.body.username != undefined && req.body.password != undefined && req.body.action == 'login') {
+		let auth = await user.passwordLogin(
+			req.body.username,
+			req.body.password);
+
+		req.login = auth;
+
+		// if logged in, set cookies
+		if (req.login.loggedIn) {
+			// store a cookie for the user_id
+			res.cookie('uid', req.login.user.user_id, { maxAge: 1000 * 60 * 60 * 24 * 5 });
+			// store as cookie for cookie code
+			res.cookie('ch', req.login.cookie, { maxAge: 1000 * 60 * 60 * 24 * 5 });
+			console.log("requested redirect:" + req.body.redirect);
+			if (req.body.redirect != undefined &&
+				req.body.redirect != '' &&
+				req.body.redirect != null &&
+				!req.body.redirect.isEmpty()
+			) {
+				res.redirect(req.body.redirect); // if redirect location specified... redirect there
+			} else {
+				res.redirect('/'); // redirect location is blank, redirect to home page
+			}
+		}
+	} else if (req.cookies.uid != undefined && req.cookies.ch != undefined) {
+		// if uid and cookie code are set, try logging in with cookies
+		let auth = await user.cookieLogin(req.cookies.uid, req.cookies.ch);
+		req.login = auth;
 	}
-	//res.send(req.login.username);
+
 	next();
 });
 
--- model/user.js
+++ model/user.js
@@ -25,5 +25,68 @@ module.exports = {
 		} else {
 			return false;
 		}
+	},
+	'logout': async function (user_id) {
+		let conn = await db.getConnection();
+		const result = await conn.query("update user set cookieHash = null where user_id = ?", [user_id]);
+		conn.end();
+		return result;
+	},
+	'cookieLogin': async function (user_id, cookie) {
+
+		let conn = await db.getConnection();
+		const cookieHash = (crypto.createHash('sha256')).update(cookie).digest('base64');
+
+		// check if the user_id and cookieHash EXIST in the database
+		const result = await conn.query("select user_id,username,email from `user` where user_id = ? and cookieHash = ?",
+			[user_id, cookieHash]);
+
+		conn.end();
+		if (result.length > 0) {
+			const ret = {
+				user: result[0],
+				loggedIn: true
+			};
+			return ret;
+		}
+		return { loggedIn: false };
+	},
+	'passwordLogin': async function (username, password) {
+		let conn = await db.getConnection();
+
+		const passHash = (crypto.createHash('sha256')).update(password).digest('base64');
+
+		const result = await conn.query("select user_id,username,email from `user` where username = ? and passHash = ?",
+			[username, passHash]);
+
+		conn.end();
+
+		if (result.length > 0) {
+
+			// a secret code, to allow the user to logijn with cookies
+			// must match the stored cookieHash, to login with cookies
+			let cookie = (crypto.createHash('sha256')).update('' + Math.random() * 99999999999999).digest('base64');
+			// hash of cookie, to be store, so when logging in with cookies:
+			// the cookie is hashed, and compared to the stored hash
+			let cookieHash = (crypto.createHash('sha256')).update(cookie).digest('base64');
+
+
+			// store the cookieHash in the database
+			const connCookie = await db.getConnection();
+			const resultCookie = await conn.query("update `user` set cookieHash = ? where user_id = ?",
+				[cookieHash, result[0].user_id]);
+
+			connCookie.end();
+
+			// return the user data, the cookie, and a flag to indicate that the user is logged in
+			const ret = {
+				user: result[0],
+				cookie: cookie,
+				loggedIn: true
+			};
+			return ret;
+		}
+
+		return { loggedIn: false };
 	}
 };
