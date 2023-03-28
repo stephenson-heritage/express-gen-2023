c45a9077676dd2dcdfec2aa0e630a26162b5c889
Lucas Stephenson <LStephenson@cegep-heritage.qc.ca>
2023-03-28 11:32:30 -0400

    added page editing

--- model/page.js
+++ model/page.js
@@ -37,5 +37,14 @@ module.exports = {
 		const result = await conn.query("select page_id,title,`key` from page where menu_order is not null order by menu_order");
 		conn.end();
 		return result;
+	},
+	'updatePage': async function (key, title, content) {
+		let conn = await db.getConnection();
+		key = key.toLowerCase();
+		const result = await conn.query(
+			"update page set title = ?, content = ? where `key` = ?",
+			[title, content, key]);
+		conn.end();
+		return result;
 	}
 };
--- routes/index.js
+++ routes/index.js
@@ -4,15 +4,30 @@ const page = require('../model/page');
 
 /* GET home page. */
 router.get('/', function (req, res, next) {
-	loadPage('welcome', req, res, next);
+	loadPage('index', 'welcome', req, res, next);
+});
+
+router.get('/:key/edit', async function (req, res, next) {
+	loadPage('editPage', req.params.key, req, res, next);
+});
+router.post('/:key/edit', async function (req, res, next) {
+
+	if (req.login.loggedIn) {
+		let title = req.body.title;
+		let content = req.body.content;
+		let key = req.body.key;
+		page.updatePage(key, title, content);
+	}
+
+	res.redirect('/' + req.params.key);
 });
 
 router.get('/:key', async function (req, res, next) {
-	loadPage(req.params.key, req, res, next);
+	loadPage('index', req.params.key, req, res, next);
 });
 
 
-let loadPage = async function (key, req, res, next) {
+let loadPage = async function (template, key, req, res, next) {
 	// eg url: localhost:9000/care
 	// key ==> care
 	const pg = await page.getPage(key); // try to retrieve the row from the model for the key "care"	
@@ -23,9 +38,10 @@ let loadPage = async function (key, req, res, next) {
 		const menuItems = await page.getMenuItems(); // retrieve the menu items
 		//console.log(menuItems);
 		// there is a matching page (with key ==> "care")
-		res.render('index', { // render template 'index.hbs', send the page title, page content
+		res.render(template, { // render template 'index.hbs', send the page title, page content
 			menu: menuItems,
 			title: pg.row.title,
+			key: pg.row.key,
 			login: req.login,
 			content: pg.row.content,
 			redirect: req.baseUrl + req.path
